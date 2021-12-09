15714080a20fa68d8a4adcd706c990ae9b669759
moniquevandennouweland <moniquevanlenthe@live.com>
2021-12-09 16:06:29 +0100

    team pagina aangepast

--- pages/team.vue
+++ pages/team.vue
@@ -4,8 +4,8 @@
     <section>
       <div class="container">
         <div class="section-heading">
-          <h2 class="display-20 display-md-18 display-lg-16">Our Team Members</h2>
-          <p class="w-95 w-md-80 w-lg-60 w-xl-55 mx-auto mb-0">Consectetur adipiscing amet lorem ipsum dolor sit elit sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+          <h2 class="display-20 display-md-18 display-lg-16">Ons team</h2>
+          <p class="w-95 w-md-80 w-lg-60 w-xl-55 mx-auto mb-0">Voor al uw klussen in en om het huis</p>
         </div>
         <div class="row justify-content-center justify-content-sm-start">
           <div class="col-lg-4 col-sm-6 col-lg-4 mb-1-6 mb-md-1-9">
@@ -58,8 +58,8 @@
                 <img src="/img/team/team-03.jpg" alt="..." class="rounded" />
               </div>
               <div class="team-info team-overlay">
-                <h3 class="team-title">Anthony Dean</h3>
-                <h6 class="team-position">Structural Architect</h6>
+                <h3 style="font-size: 16px;" class="team-title">Monique van den Nouweland</h3>
+                <h6 class="team-position">Eigenaar, klusvrouw</h6>
                 <ul class="social-icon">
                   <li>
                     <a href="#!"><i class="fab fa-facebook-f"></i></a>
