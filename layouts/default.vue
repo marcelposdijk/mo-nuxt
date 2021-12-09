184b7b4d9b3e115bdc6438947af06ce78a04defa
Marcel Posdijk <mposdijk@marpos.nl>
2021-12-09 15:34:23 +0100

    logo footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -86,7 +86,7 @@
         <div class="row">
           <div class="col-lg-3 col-md-6 mb-2-5 mb-lg-0">
             <div class="mb-1-6">
-              <img src="img/logos/logo-footer.png" alt="..." />
+              <img src="/img/logos/logo-footer.png" alt="..." />
             </div>
           </div>
           <div class="col-lg-3 col-md-6 mb-2-5 mb-lg-0">
