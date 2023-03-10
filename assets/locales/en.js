352821c880ea8d3c9562458bbad5bed654f5881e
moniquevandennouweland <moniquevanlenthe@live.com>
2023-03-10 13:23:31 +0100

    engelse vertaling

--- assets/locales/en.js
+++ assets/locales/en.js
@@ -4,7 +4,7 @@ export default {
   welcome: 'Welcome',
   by: "by",
   goBack: "Go back",
-  slogan: "en => Voor al uw klussen in en om het huis!",
+  slogan: "en => For all your jobs in and around the house!",
   main: {
     Home: "Home",
     MijnTeam: "My Team",
@@ -15,31 +15,31 @@ export default {
     LaatsteProjecten: "Latest projects",
     Adres: "Address",
     Telefoon: "Phone",
-    OpeningsDagen: "en => Maandag t/m vrijdag",
-    OpeningsTijden: "en => 8:00 tot 16:00 uur",
+    OpeningsDagen: "en => Monday to Friday",
+    OpeningsTijden: "en => 8:00 am to 4:00 pm",
     Email: "en => E-mail"
   },
   home: {
-    JarenKlusErvaring: "en => Al jaren kluservaring",
-    BetrouwbareKlusvrouw: "en => Betrouwbare Klusvrouw/Schilder",
-    Text: "en => Hallo, ik ben Klusvrouw Monique. Wat leuk dat je mijn website bezoekt. Na 26,5 jaar werkzaam te zijn geweest in de financiële wereld, heb ik mij om laten scholen tot klusvrouw. Mijn passie is creatief bezig zijn, iets moois maken. Ik heb mijn hele leven al thuis geklust, bij familie en vrienden. In overleg met de klant wordt er gekeken wat de wensen zijn. Er kan een interieuradvies worden gedaan en dit kan uitgevoerd worden. Alles wordt besproken en gaat in overleg. Hieronder tref je de diensten aan die ik aanbied.",
-    DienstenDieIkAanbied: "en => Diensten die ik aanbied",
-    DeLaatsteProjecten: "en => De laatste projecten",
-    MijnWerkProces: "en => Mijn werkproces",
-    EersteContactVia: "en => Eerste contact via mobiel, mail of whatsapp",
-    NetjesKundig: "en => Netjes, kundig en betrouwbaar",
-    WensenEnPlanning: "en => Wensen en planning",
+    JarenKlusErvaring: "en => Many years of job experience",
+    BetrouwbareKlusvrouw: "en => Reliable Handywoman/Painter",
+    Text: "en => Hello, I am Handyman Monique. How nice of you to visit my website. After 26.5 years working in the financial world, I have been retrained as a handywoman. My passion is being creative, making something beautiful. I have been doing odd jobs at home, with family and friends all my life. In consultation with the customer, we look at what the wishes are. An interior advice can be made and carried out. Everything is discussed and done in consultation. Below are the services I offer.",
+    DienstenDieIkAanbied: "en => Services I provide",
+    DeLaatsteProjecten: "en => The latest projects",
+    MijnWerkProces: "en => My work process",
+    EersteContactVia: "en => First contact via mobile, e-mail or whatsapp",
+    NetjesKundig: "en => Neat, competent and reliable",
+    WensenEnPlanning: "en => Wishes and planning",
     MijnTeam: "My Team",
-    Uitvoering: "en => Uitvoering",
-    SchilderwerkBinnen: "en => Schilderwerk binnen",
-    Benhangklussen: "en => Behangklussen",
-    KlussenInDeTuin: "en => Klussen in de tuin",
-    Timmerwerk: "en => Timmerwerk",
-    SchilderwerkBuiten: "en => Schilderwerk buiten"
+    Uitvoering: "en => Execution",
+    SchilderwerkBinnen: "en => Interior painting",
+    Benhangklussen: "en => Wallpaper jobs",
+    KlussenInDeTuin: "en => Jobs in the garden",
+    Timmerwerk: "en => Carpentry",
+    SchilderwerkBuiten: "en => Exterior painting"
   },
   diensten: {
-    title: "en => Diensten",
-    DienstenDieIkAanbied: "en => Diensten die ik aanbied"
+    title: "en => Services",
+    DienstenDieIkAanbied: "en => Services that I offer"
   },
   notfound: {
     title: "Oops! page not found",
@@ -47,31 +47,31 @@ export default {
   },
   contact: {
     title: "Contact",
-    description: "en => Zo kom je met mij in contact",
-    telefoon: "en => Telefoon",
-    kantoor: "en => Kantoor",
+    description: "en => How to get in touch with me",
+    telefoon: "en => Telephone",
+    kantoor: "en => Office",
     email: "en => E-mail",
-    IkHoorGraagVanJe: "en => Ik hoor graag van je",
-    Naam: "en => Naam",
-    EmailAdres: "en => E-mailadres",
-    Onderwerp: "en => Onderwerp",
-    Telefoonnummer: "en => Telefoonnummer",
-    Bericht: "en => Bericht",
-    BerichtVerzenden: "en => Bericht verzenden",
-    BedanktVoorJeBericht: "en => Dank je wel voor je bericht. Ik neem zo snel mogelijk contact met je op",
-    NaamIsVerplicht: "en => NaamIsVerplicht",
-    EmailAddressIsVerplicht: "en => E-mailadres is verplicht",
-    BerichtIsVerplicht: "en => Bericht is verplicht",
-    IkHoorGraagVanJe: "en => Ik hoor graag van je.",
-    StelJeVraag: "en => Stel je vraag in een aantal woorden"
+    IkHoorGraagVanJe: "en => I look forward to hearing from you",
+    Naam: "en => Nane",
+    EmailAdres: "en => E-mail address",
+    Onderwerp: "en => Subject",
+    Telefoonnummer: "en => Telephone number",
+    Bericht: "en => Message",
+    BerichtVerzenden: "en => Send message",
+    BedanktVoorJeBericht: "en => Thank you for your message. I will get back to you as soon as possible",
+    NaamIsVerplicht: "en => Name is required",
+    EmailAddressIsVerplicht: "en => E-mail address is required",
+    BerichtIsVerplicht: "en => Message is required",
+    IkHoorGraagVanJe: "en => I look forward to hearing from you.",
+    StelJeVraag: "en => Ask your question in a few words"
   },
   team: {
     title: "My team",
-    description: "Mijn team staat voor u klaar voor al uw klussen in en om het huis!"
+    description: "My team is at your service for all your jobs in and around the house!"
   },
   portfolio: {
     title: "Portfolio",
-    description: "Een impressie van de door onse uitgevoerde projecten op het gebied van timmerwerk, schilderwerk en behangen"
+    description: "An impression of the carpentry, painting and wallpapering projects carried out by us"
   },
   notfound: {
     title: "Oops! page not found",
